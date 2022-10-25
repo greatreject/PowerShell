e898416056ac8798ad8ca571123cfff085f3c190
greatreject <116633783+greatreject@users.noreply.github.com>
2022-10-25 11:50:10 +0100

    Add files via upload

---
+++ DisableAccountsAfter30Days.ps1
@@ -0,0 +1,2 @@
+﻿ $searchBase = "OU=UAT,OU=Accounts,OU=AWS,DC=LEO,DC=INFRA,DC=COM"
+ Search-ADAccount -SearchBase $searchBase -UsersOnly -AccountInactive -TimeSpan ([timespan]30d) | Set-ADUser -Enabled $false
